d9a3f9fcb21c3af0bc68ece544826c2d9914029a
Imagine King <janiukbartosz@gmail.com>
2021-02-22 19:11:15 +0100

    Non responsive design

--- Desktop/InstaBUD/script.js
+++ Desktop/InstaBUD/script.js
@@ -7,3 +7,29 @@ hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('open');
   lineBurger.classList.toggle('open');
 });
+
+const circles = document.querySelectorAll('.experience__tab__circle');
+const counters = document.querySelectorAll('.experience__tab__counter');
+const speed = 20;
+
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+    const inc = Math.floor(target / speed);
+    // console.log(`Count:` + count);
+    // console.log(`Target:` + target);
+    if (count < target) {
+      counter.innerText = count + inc;
+      setTimeout(updateCount, 100);
+    } else {
+      // count.innerText = '+';
+      // circles.forEach(
+      //   circle =>
+      //     (circle.style.backgroundImage = 'linear-gradient(62deg, #fd5d06 0%, #ffda8d 100%)')
+      // );
+      circles.forEach(circle => circle.classList.add('experience__tab__circle--loaded'));
+    }
+  };
+  updateCount();
+});
